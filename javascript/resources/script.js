26a631d9621e018fb27fc9a630285d2c6143c4be
camilagoulartsoares <camilagoulartsoares@yahoo.com>
2024-11-10 20:10:26 -0300

    .

--- javascript/resources/script.js
+++ javascript/resources/script.js
@@ -16,6 +16,32 @@ const product = {
     name: 'Potato',
     quantity: 100
 }
-console.log(product.quantity)
+//console.log(product.quantity)
 product.quantity = 90
-console.log(product.quantity)
+//console.log(product.quantity)
+
+
+/** *********************COALESCÊNCIA NULA************************************** */
+//Operador lógico que retorna o seu operando do lado direito quando o operador do lado esquerdo é null ou undefined. Caso contrário,ele retorna o lado esquerdo
+
+let content = null;
+//console.log(content ?? 'Conteudo padrão')
+
+const userSchool = {
+    name: 'Camila',
+    picture: 'null'
+}
+
+//console.log(userSchool.picture ?? 'Nao possui foto ainda')
+
+
+/** *********************FUNÇÃO CONSTRUTORA ************************************** */
+function createProduct(name) {
+    const products = {}
+
+    products.name = name
+    console.log(products)
+}
+
+createProduct('Banana')
+

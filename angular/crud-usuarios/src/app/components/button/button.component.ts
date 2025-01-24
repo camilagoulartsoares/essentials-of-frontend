4a25bb77d531b50cbb75b8bbba49517727d63614
camilagoulartsoares <camilagoulartsoares@yahoo.com>
2025-01-23 21:36:51 -0300

    .

--- angular/crud-usuarios/src/app/components/button/button.component.ts
+++ angular/crud-usuarios/src/app/components/button/button.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   standalone: false,
-  
+
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
-
+@Input() text!: string;
 }

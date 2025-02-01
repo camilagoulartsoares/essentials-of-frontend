28f988d6b79bbe9dc0041484e66c2d15495aebcd
camilagoulartsoares <camilagoulartsoares@yahoo.com>
2025-01-31 21:35:03 -0300

    .

--- angular/crud-usuarios/src/app/components/button/button.component.ts
+++ angular/crud-usuarios/src/app/components/button/button.component.ts
@@ -9,4 +9,6 @@ import { Component, Input } from '@angular/core';
 })
 export class ButtonComponent {
 @Input() text!: string;
+
+@Input() size: string = 'auto'
 }
--- angular/crud-usuarios/src/app/pages/login/login.component.ts
+++ angular/crud-usuarios/src/app/pages/login/login.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: false,
-  
+
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  username: string;
 
+  constructor(private route: Router){}
+
+  login(){
+   sessionStorage.setItem('user', this.username)
+
+    this.route.navigate(['home'])
+  }
 }

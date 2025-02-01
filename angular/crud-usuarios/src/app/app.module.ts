74475403aa5397df2ba4531cfb571858bdbb5b26
camilagoulartsoares <camilagoulartsoares@yahoo.com>
2025-02-01 17:45:43 -0300

    .

--- angular/crud-usuarios/src/app/app.module.ts
+++ angular/crud-usuarios/src/app/app.module.ts
@@ -6,18 +6,26 @@ import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { ButtonComponent } from './components/button/button.component';
 import { LoginComponent } from './pages/login/login.component';
-import { FormsModule} from '@angular/forms'
+import { FormsModule} from '@angular/forms';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './components/menu/menu.component'
+import { MatIconModule } from '@angular/material/icon'
+import { RouterLink } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     ButtonComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    MatIconModule,
+    RouterLink
   ],
   providers: [
     provideAnimationsAsync()

24bf11be566e62465e0d2b392d7e0ed7da47217d
camilagoulartsoares <camilagoulartsoares@yahoo.com>
2025-02-05 14:44:08 -0300

    .

--- angular/udemy-max/first-angular-app/src/app/app.component.ts
+++ angular/udemy-max/first-angular-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./components/header.component"
+import { HeaderComponent } from "./components/header/header.component"
+import { UsersComponent } from "./components/users/users.component";
 
 @Component({
   selector: 'app-root', // a tag que chamamos quando queremos usar esse componente em outros lugares
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, UsersComponent],
   templateUrl: './app.component.html', // html a ser mostrado desse componente
   styleUrl: './app.component.css' // a estilização desse componente
 })
--- angular/udemy-max/first-angular-app/src/app/components/users/users.component.ts
+++ angular/udemy-max/first-angular-app/src/app/components/users/users.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { DUMMY_USERS } from './dummy-users';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.css'
+  styleUrl: './users.component.css',
 })
 export class UsersComponent {
+  public users = signal(DUMMY_USERS)
 
+  ngOnInit() {
+    console.log(this.users);
+  }
+
+  teste(event: any) {
+    console.log(event)
+  }
 }

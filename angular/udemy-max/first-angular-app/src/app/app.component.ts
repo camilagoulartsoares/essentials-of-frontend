ebdae5da9ec2606eeb2ad3797243ea2fb5e07b80
camilagoulartsoares <camilagoulartsoares@yahoo.com>
2025-02-19 18:09:46 -0300

    .

--- angular/udemy-max/first-angular-app/src/app/app.component.ts
+++ angular/udemy-max/first-angular-app/src/app/app.component.ts
@@ -1,20 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./components/header/header.component"
-import { UsersComponent } from "./components/users/users.component";
+import { HeaderComponent } from './components/header/header.component';
+import { UsersComponent } from './components/users/users.component';
 import { DUMMY_USERS } from './components/users/dummy-users';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @Component({
   selector: 'app-root', // a tag que chamamos quando queremos usar esse componente em outros lugares
-  imports: [RouterOutlet, HeaderComponent, UsersComponent],
+  imports: [RouterOutlet, HeaderComponent, UsersComponent, TasksComponent],
   templateUrl: './app.component.html', // html a ser mostrado desse componente
-  styleUrl: './app.component.css' // a estilização desse componente
+  styleUrl: './app.component.css', // a estilização desse componente
 })
 export class AppComponent {
- users = DUMMY_USERS;
+  users = DUMMY_USERS;
+  selectedUserId = 'u1';
 
- onSelectUser(id: string){
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
 
-  console.log('clicado',id)
- }
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
 }

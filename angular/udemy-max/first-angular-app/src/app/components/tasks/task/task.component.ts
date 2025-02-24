f89da7842089db82ce2cfff7057fc938b3af5b48
camilagoulartsoares <camilagoulartsoares@yahoo.com>
2025-02-24 19:01:28 -0300

    .

--- angular/udemy-max/first-angular-app/src/app/components/tasks/task/task.component.ts
+++ angular/udemy-max/first-angular-app/src/app/components/tasks/task/task.component.ts
@@ -1,6 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task } from './tasks.mode';
-
 
 @Component({
   selector: 'app-task',
@@ -10,8 +9,9 @@ import { Task } from './tasks.mode';
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
+  @Output() complete = new EventEmitter<string>();
 
-  ngOnInit(){
-    console.log(this.task)
+  onCompleteTask() {
+    this.complete.emit(this.task.id)
   }
 }

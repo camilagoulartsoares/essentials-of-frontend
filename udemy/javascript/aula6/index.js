65568844ff092bf965d56f8f14c091aa372e9d87
Camila Soares <camilagoulartsoares@yahoo.com>
2025-05-14 20:14:39 -0300

    .

---
+++ udemy/javascript/aula6/index.js
@@ -0,0 +1,8 @@
+let names;
+console.log(names) // resultado: UNDEFINED
+
+//Não pode redeclarar variável
+//Pode ser criada sem valor e depois atribuir um valor:
+
+names = 'maria'
+console.log(names) // resultado: maria

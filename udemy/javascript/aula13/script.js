f61700408455c3f608951c6f5d7923daf3d53b94
Camila Soares <camila.soares@gfinnovation.com.br>
2025-05-21 11:55:09 -0300

    .

--- udemy/javascript/aula13/script.js
+++ udemy/javascript/aula13/script.js
@@ -1,3 +1,10 @@
-let umaString = 'Texo "aqui"'
+let umaString = "Um texto"
+// console.log(umaString.charAt(4))
 
-console.log(umaString)
+// console.log(umaString.replace('Um', 'Outro'))
+
+//SLICE
+
+let texto = 'O rato roeu a roupa do rei de roma.'
+
+console.log(texto.slice(2,6)) //no slice envia onde começa e onde termina a tirar a palavra

90a8211603c00def19bff7f4c25a69c27163d63b
Camila Soares <camilagoulartsoares@yahoo.com>
2025-05-21 20:53:23 -0300

    .

---
+++ udemy/javascript/aula14/script.js
@@ -0,0 +1,12 @@
+//Convertendo para string
+//let num1 = 1
+
+// console.log(num1.toString());
+
+//Arredondando número pra cima
+// let num2 = 10.5904803930
+// console.log(num2.toFixed(2))
+
+//Verificar se um número é inteiro
+let num1 = 10.4
+console.log(Number.isInteger())
---
+++ udemy/javascript/aula15/script.js
@@ -0,0 +1,19 @@
+//ARREDONDAR NÚMERO PARA BAIXO
+
+let num1 = 9.53904;
+let num2 = Math.floor(num1)
+// console.log(num2)
+
+//ARREDONDAR NÚMERO PARA CIMA
+
+let num3 = Math.ceil(num1)
+// console.log(num3)
+
+//ENCONTRAR O MENOR NÚMERO DA SEQUÊNCIA
+// console.log(Math.min(1,23,4,64,30,-1))
+
+//ENCONTRAR O MAIOR NÚMERO DA SEQUÊNCIA
+//  console.log(Math.max(1,23,4,64,30,-1))
+
+ //GERAR NÚMERO ALEATÓRIO
+ console.log(Math.random())

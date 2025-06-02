726c66449477f5ad5ff17d0b25f7538949478d48
Camila Soares <camilagoulartsoares@yahoo.com>
2025-06-02 20:46:43 -0300

    .

--- rocketseat/angular/gerador-certificado/src/app/app.component.ts
+++ rocketseat/angular/gerador-certificado/src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./_components/navbar/navbar.component";
 import { PrimaryButtonComponent } from './_components/primary-button/primary-button.component';
+import { SecondaryButtonComponent } from "./_components/secondary-button/secondary-button.component";
+import { ItemCertificadoComponent } from './_components/item-certificado/item-certificado.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ NavbarComponent,PrimaryButtonComponent],
+  imports: [NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent,ItemCertificadoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

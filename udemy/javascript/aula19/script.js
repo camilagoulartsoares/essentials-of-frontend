0ad260c9ec9d3f1d8aa9dd4fc5da96ba4fe6a50e
Camila Soares <camila.soares@gfinnovation.com.br>
2025-06-05 14:38:02 -0300

    .

---
+++ udemy/javascript/aula19/script.js
@@ -0,0 +1,20 @@
+// let nome = 'Luiz'
+// nome[0] = 'R';
+// console.log(nome[0])
+
+// let a = [1, 2, 3];
+// let b = a;
+// b = [1]
+// console.log(a,b)
+
+
+const a  = {
+    nome: 'Luiz',
+    sobrenome: 'Otávio'
+}
+
+let b = a;
+
+b = 'Luiz'
+
+console.log(a,b)

875fc2f095638f090e3c181a98b9394d9c9dbca9
Camila Soares <camila.soares@gfinnovation.com.br>
2025-06-05 17:04:54 -0300

    form

--- udemy/javascript/aula20/script.js
+++ udemy/javascript/aula20/script.js
@@ -1,5 +1,21 @@
-function meuEscopo () {
-    let nome = 'Luiz'
+function meuEscopo() {
+  const form = document.querySelector(".form");
+
+//   form.onsubmit = function (evento) {
+//     evento.preventDefault();
+//   };
+
+    function recebeEventoForm (event) {
+        event.preventDefault();
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const altura = form.querySelector('.altura');
+        const peso = form.querySelector('.peso');
+
+        console.log(nome.value,sobrenome,peso,altura)
+    }
+
+    form.addEventListener('submit', recebeEventoForm);
 }
 
-meuEscopo()
+meuEscopo();

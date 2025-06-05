dd0eae5d5984f063a908421fac755c21212f8dae
Camila Soares <camila.soares@gfinnovation.com.br>
2025-06-05 17:32:27 -0300

    .

--- udemy/javascript/aula20/script.js
+++ udemy/javascript/aula20/script.js
@@ -4,30 +4,30 @@ function meuEscopo() {
 
   let pessoas = [];
 
-//   form.onsubmit = function (evento) {
-//     evento.preventDefault();
-//   };
+  //   form.onsubmit = function (evento) {
+  //     evento.preventDefault();
+  //   };
 
-    function recebeEventoForm (event) {
-        event.preventDefault();
-        const nome = form.querySelector('.nome');
-        const sobrenome = form.querySelector('.sobrenome');
-        const altura = form.querySelector('.altura');
-        const peso = form.querySelector('.peso');
+  function recebeEventoForm(event) {
+    event.preventDefault();
+    const nome = form.querySelector(".nome");
+    const sobrenome = form.querySelector(".sobrenome");
+    const altura = form.querySelector(".altura");
+    const peso = form.querySelector(".peso");
 
-        pessoas.push({
-            nome: nome.value,
-            sobrenome: sobrenome.value,
-            altura: altura.value,
-            peso: peso.value
-        })
+    pessoas.push({
+      nome: nome.value,
+      sobrenome: sobrenome.value,
+      altura: altura.value,
+      peso: peso.value,
+    });
 
-        console.log(pessoas)
+    console.log(pessoas);
 
-        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${altura.value} ${peso.value}</p>`
-    }
+    resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${altura.value} ${peso.value}</p>`;
+  }
 
-    form.addEventListener('submit', recebeEventoForm);
+  form.addEventListener("submit", recebeEventoForm);
 }
 
 meuEscopo();

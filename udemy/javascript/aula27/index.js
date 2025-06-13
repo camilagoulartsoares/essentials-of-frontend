43543fccff5064c9f733dc0e58db1cc57b5977ff
Camila Soares <camilagoulartsoares@yahoo.com>
2025-06-13 08:10:22 -0300

    Op ternaria

--- udemy/javascript/aula27/index.js
+++ udemy/javascript/aula27/index.js
@@ -1,3 +1,8 @@
 const usuarioPontos = 99
 const pontuacao = usuarioPontos >= 90 
-console.log(pontuacao ? 'maior que 90' : 'Menor' )
+// console.log(pontuacao ? 'maior que 90' : 'Menor' )
+
+
+const pontuacaoUsuario = 999;
+
+console.log(pontuacaoUsuario >= 1000 ? 'VIP': 'Não é vip')

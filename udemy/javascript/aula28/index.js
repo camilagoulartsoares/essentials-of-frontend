868bcad60cc20a498f26d1d4ec7edc881255f620
Camila Soares <camilagoulartsoares@yahoo.com>
2025-06-14 07:37:52 -0300

    .

--- udemy/javascript/aula28/index.js
+++ udemy/javascript/aula28/index.js
@@ -1,8 +1,14 @@
-const data = new Date()
 
 // console.log(data)
 
 //Objeto date
 
-const dataPassada = new Date(0)
-console.log(dataPassada)
+// const dataPassada = new Date(0)
+// console.log(dataPassada)
+
+const data = new Date('2019-04-20')
+
+console.log('DIA',data.getDate());
+console.log('Mês',data.getMonth());
+console.log('Ano',data.getFullYear());
+console.log('HORA',data.getHours())

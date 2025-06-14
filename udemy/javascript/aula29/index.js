30621617abfcb76680a3504247f642a2f7f984ce
Camila Soares <camilagoulartsoares@yahoo.com>
2025-06-14 09:35:45 -0300

    swtich case

---
+++ udemy/javascript/aula29/index.js
@@ -0,0 +1,21 @@
+// const data = new Date('2020-04-21 00:00:00')
+
+
+// const diaSemana = data.getDay()
+// console.log(diaSemana)
+
+switch (diaSemana) {
+    case  0:
+    console.log("Segunda feira");
+    break
+
+    case 1:
+    console.log("Terça feira")    
+    break
+
+    default:
+    console.log("Dia semana ")
+}
+
+const data = new Date("2020-04-22 00:00:00")
+const diaSemana = data.getDay()

8835834a4b7a76fdcd794b212915f003cdfe1985
Camila Soares <camilagoulartsoares@yahoo.com>
2025-06-14 19:21:39 -0300

    outra forma ex

--- udemy/javascript/aula30-demo/assets/index.js
+++ udemy/javascript/aula30-demo/assets/index.js
@@ -1,61 +1,71 @@
-const pegaDiv = document.querySelector("#data")
+// const pegaDiv = document.querySelector("#data")
 
-const dataAtual = new Date()
-console.log(dataAtual)
+// const dataAtual = new Date()
+// console.log(dataAtual)
 
 
-const dia = dataAtual.getDay()
-const mes = dataAtual.getMonth()
-const ano = dataAtual.getFullYear()
-const diaDeHoje = dataAtual.getDate()
+// const dia = dataAtual.getDay()
+// const mes = dataAtual.getMonth()
+// const ano = dataAtual.getFullYear()
+// const diaDeHoje = dataAtual.getDate()
 
-const hora = dataAtual.getHours();
-const minuto = dataAtual.getMinutes()
+// const hora = dataAtual.getHours();
+// const minuto = dataAtual.getMinutes()
 
-function formataHoras (minuto) {
-    return minuto >= 10 ? '0' : '00'
+// function formataHoras (minuto) {
+//     return minuto >= 10 ? minuto : '0${minuto}'
+// }
+
+// const minFormatado = formataHoras(minuto)
+
+
+// function transformaNumeroEmDia(data) {
+//     switch (data) {
+//         case 0:
+//             return 'Segunda'
+//         case 1:
+//             return 'Terça feira'
+//         case 2:
+//             return 'Quarta feira'
+//         case 3:
+//             return 'Quinta feira'
+//         case 4:
+//             return 'sexta'
+//         case 5:
+//             return 'sabado'
+//         default:
+//             return 'Domingo'
+//     }
+// }
+
+// function transformaNumeroEmMes(data) {
+//     switch (data) {
+//         case 0:
+//             return 'Janeiro'
+//         case 1:
+//             return 'Fev'
+//         case 2:
+//             return 'Março'
+//         case 3:
+//             return 'Abril'
+//         case 4:
+//             return 'Maio'
+//         case 5:
+//             return 'Junho'
+//         default:
+//             return 'Julho'
+//     }
+// }
+// const transformaODia = transformaNumeroEmDia(mes)
+// const transformaNumEmMes = transformaNumeroEmMes(mes)
+// const mostraNaTela = pegaDiv.innerHTML = `<strong>${transformaODia}, ${diaDeHoje} de ${transformaNumEmMes} de ${ano} às ${hora}:${minFormatado}</strong>`
+
+
+//Outra forma de mostrar data
+const pegaDiv = document.querySelector("#data");
+const dataAtual = new Date();
+const opcoes = {
+    dateStyle: 'full',
+    // timeStyle: 'short'
 }
-
-const minFormatado = formataHoras(minuto)
-
-
-function transformaNumeroEmDia(data) {
-    switch (data) {
-        case 0:
-            return 'Segunda'
-        case 1:
-            return 'Terça feira'
-        case 2:
-            return 'Quarta feira'
-        case 3:
-            return 'Quinta feira'
-        case 4:
-            return 'sexta'
-        case 5:
-            return 'sabado'
-        default:
-            return 'Domingo'
-    }
-}
-
-function transformaNumeroEmMes(data) {
-    switch (data) {
-        case 0:
-            return 'Janeiro'
-        case 1:
-            return 'Fev'
-        case 2:
-            return 'Março'
-        case 3:
-            return 'Abril'
-        case 4:
-            return 'Maio'
-        case 5:
-            return 'Junho'
-        default:
-            return 'Julho'
-    }
-}
-const transformaODia = transformaNumeroEmDia(mes)
-const transformaNumEmMes = transformaNumeroEmMes(mes)
-const mostraNaTela = pegaDiv.innerHTML = `<strong>${transformaODia}, ${diaDeHoje} de ${transformaNumEmMes} de ${ano} às ${hora}:${minuto}</strong>`
+pegaDiv.innerHTML= dataAtual.toLocaleDateString('pt-BR',opcoes)

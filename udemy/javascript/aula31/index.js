125ade7663ef6ee305ab6bc54b67641892ba59aa
Camila Soares <camilagoulartsoares@yahoo.com>
2025-06-15 14:41:13 -0300

    aula 31

---
+++ udemy/javascript/aula31/index.js
@@ -0,0 +1,13 @@
+let nome = 'Luiz'
+var nome2 = 'Luiz'
+const verdadeiro = true;
+
+console.log(nome , "fora do if")
+
+//Let tem escopo de bloco {.....}
+
+if (verdadeiro) {
+    let nome = 'Camila'
+    console.log("dentro do if",nome)
+}
+

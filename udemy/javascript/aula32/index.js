48c96b2c2754f5f1cf0cbaa541e6bf4d5c5e9488
Camila Soares <camilagoulartsoares@yahoo.com>
2025-06-15 15:58:35 -0300

    desestruturação array

---
+++ udemy/javascript/aula32/index.js
@@ -0,0 +1,10 @@
+// let a = 'A';
+// let b = 'B';
+// let c = 'C';
+
+// console.log(a,b,c)
+
+const numeros = [1000,200,300,4,5,6,7]
+const [primeiroNumero,segundoNumero] = numeros;
+
+console.log(primeiroNumero,segundoNumero)

c2b6391c02e999083b9fb52c010d1f2f7c7639ae
Camila Soares <camilagoulartsoares@yahoo.com>
2025-06-16 08:04:00 -0300

    for

--- udemy/javascript/aula34/index.js
+++ udemy/javascript/aula34/index.js
@@ -1,5 +1,12 @@
 //i = index
 
-for (let i = 0; i <= 5;i++) {
-    console.log(`Linha ${i}`)
+// for (let i = 0; i <= 5;i++) {
+//     console.log(`Linha ${i}`)
+// }
+
+
+const frutas = ['Maca', 'banana', 'pera']
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i])
 }

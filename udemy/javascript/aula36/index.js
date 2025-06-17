9d33d09b73689cc9f5b06a63821c181e69552622
Camila Soares <camilagoulartsoares@yahoo.com>
2025-06-17 06:35:41 -0300

    aula 36

---
+++ udemy/javascript/aula36/index.js
@@ -0,0 +1,16 @@
+const frutas = ['banana','pera','maca']
+
+
+for (let i in frutas) {
+    console.log(frutas[i])
+}
+
+const pessoa = {
+    nome: 'Camila',
+    age: 10
+
+}
+
+for (let chave in pessoa){
+    console.log(pessoa[chave])
+}

b0d11ab2ad47a143c53fb6a4b4f1624bee34231c
Camila Soares <camilagoulartsoares@yahoo.com>
2025-06-21 11:12:41 -0300

    nodelist

---
+++ udemy/javascript/aula38/assets/index.js
@@ -0,0 +1,14 @@
+const paragrafo = document.querySelector(".paragrafos")
+const ps = paragrafo.querySelectorAll('p')
+
+const estilosBody = getComputedStyle(document.body)
+const backgroundColorBody = estilosBody.backgroundColor
+
+
+for (let p of ps){
+    p.style.backgroundColor = backgroundColorBody;
+   
+}
+
+
+

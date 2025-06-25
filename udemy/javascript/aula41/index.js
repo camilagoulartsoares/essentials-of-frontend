7535e2308a444a2fc53be80c5ed892b25510acad
Camila Soares <camilagoulartsoares@yahoo.com>
2025-06-25 07:24:51 -0300

    aula 41 p2

--- udemy/javascript/aula41/index.js
+++ udemy/javascript/aula41/index.js
@@ -1,7 +1,11 @@
 function recebeNumeros (min,max) {
-    if(max > min){
-        return console.log(max)
-    } 
+    // if(max > min){
+    //     return console.log(max)
+    // } 
+    // return console.log(min)
+
+    //se o numero max for maior que min retorna max,se nao retorna o min. Esses dois blocos de códigos são a mesma coisa
+    if(max > min) return console.log(max);
     return console.log(min)
     
 }

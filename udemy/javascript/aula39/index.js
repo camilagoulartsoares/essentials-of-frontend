02da3446632c895629e17e8fb5509790b12c4a49
Camila Soares <camilagoulartsoares@yahoo.com>
2025-06-25 06:33:26 -0300

    while e do while

--- udemy/javascript/aula39/index.js
+++ udemy/javascript/aula39/index.js
@@ -1,6 +1,23 @@
 let i = 0;
-//WHILE é para quando nao sabemos ate quando ai acontecer o laço
+//WHILE é para quando nao sabemos ate quando ai acontecer o laço. Ele checa a condição e executa o laço
 while(i <= 10) {
     console.log(i)
     i++
 }
+
+function random(min,max) {
+    const r = Math.random() * (max-min) + min;
+    return Math.floor(r)
+}
+
+const min = 1
+const max = 50
+let rand = random(min,max)
+console.log(rand)
+
+while (rand !== 10) {
+    rand = random(min,max)
+    console.log(rand)
+}
+
+//o DO WHILE executa a função e depois checa

ee6da13032d3ffb0eeb3dd87ceca1cb6d671211d
Camila Soares <camilagoulartsoares@yahoo.com>
2025-06-25 06:50:29 -0300

    p2 aula 40

--- udemy/javascript/aula40/index.js
+++ udemy/javascript/aula40/index.js
@@ -1,9 +1,17 @@
 const numeros = [1,2,3,4,5,6,7,8,9]
 
 for (let numero of numeros){
+
     if (numero === 2) {
         console.log('Pulei o numero 2')
-        continue;
+        continue; //Continue continua para a próxima iteração
     }
+
+    if(numero === 7) {
+        console.log("7 encontrado,saindo...")
+        break; //Quando encontra o break ele para o laço, sai do laço
+    }
+
     console.log(numero)
 }
+

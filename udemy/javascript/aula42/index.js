2ebd1ac5c5f8bd4f76de64e888a1a7746cfc1035
Camila Soares <camilagoulartsoares@yahoo.com>
2025-06-26 07:15:55 -0300

    p2 aula 42

--- udemy/javascript/aula42/index.js
+++ udemy/javascript/aula42/index.js
@@ -2,4 +2,21 @@ try {
    console.log(existe) 
 } catch(error) {
     console.log('nao existe')
+    // console.log(error)
 }
+
+function soma(x, y) {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+        throw ("X e Y precisam ser numeros");
+    }
+
+    return x + y;
+}
+
+try {
+console.log(soma(1, 2));
+console.log(soma('1',2))
+} catch(error) {
+    console.log(error)
+}
+

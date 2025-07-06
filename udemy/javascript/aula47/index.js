c2fe3374ba434d0a1666abc2cde2d3a5edae2218
Camila Soares <camilagoulartsoares@yahoo.com>
2025-07-06 16:17:36 -0300

    class 47

---
+++ udemy/javascript/aula47/index.js
@@ -0,0 +1,20 @@
+//Declaração de função (Function Hoisting)
+
+function falaOi() {
+    console.log("OI")
+}
+
+//First-class objects (Objetos de primeira classe)
+//Function expression
+
+const souUmDado = function(){
+    console.log('Sou um dado')
+}
+
+souUmDado()
+
+//Arrow function
+
+const arrowFunction = () => {
+    
+}

e2cd6c3ad514f711528a67d95f14d6a82721ccb0
Camila Soares <camilagoulartsoares@yahoo.com>
2025-07-15 07:05:47 -0300

    function

--- udemy/javascript/aula56/index.js
+++ udemy/javascript/aula56/index.js
@@ -1,2 +1,10 @@
 // Função construtora retorna objetos
 // Função fábrica fabrica objetos
+
+function Pessoa(nome,sobrenome) {
+    this.nome = nome
+    this.sobrenome = sobrenome
+}
+
+const p1 = new Pessoa('Luiz','Otavio')
+console.log(p1)

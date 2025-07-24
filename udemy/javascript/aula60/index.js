a2d86362e3f91244fe29e4e56606a3cfb7a1cd45
Camila Soares <camilagoulartsoares@yahoo.com>
2025-07-24 05:51:44 -0300

    arrays

---
+++ udemy/javascript/aula60/index.js
@@ -0,0 +1,12 @@
+// //              0           1       2
+// const nomes = ['Eduardo','Maria','Joana']
+// nomes.push('Joao') //Adicionando elementos no final do array
+// // console.log(nomes)
+// // const nomes = new Array('Eduardo','Ana') //menos usado
+
+// nomes.unshift('Wallace')
+// console.log(nomes)
+
+const nomes = [ 'Ana', 'Alice' ]
+const transformaString = nomes.join(' ') //Ana Alice
+console.log(transformaString)

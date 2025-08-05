8ddf193cdc820d468e57e18a9c49a358264cefd9
Camila Soares <camilagoulartsoares@yahoo.com>
2025-08-05 06:40:15 -0300

    .

---
+++ udemy/javascript/aula64/index.js
@@ -0,0 +1,5 @@
+//filter,map,reduce
+
+const numeros = [5,50,40,5,7,8,60,70,43,53,54,34,657]
+const filtrados = numeros.filter(num => num > 10)
+console.log(filtrados)
---
+++ udemy/javascript/revisions/splice/index.js
@@ -0,0 +1,6 @@
+//Dado o array abaixo, remova o item "banana" e adicione "abacaxi" e "morango" no lugar.
+
+const frutas = ["maçã", "banana", "laranja", "uva"];
+frutas.splice(1,1,'Abacaxi','Morango')
+
+console.log(frutas)

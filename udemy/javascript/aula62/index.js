2f5473879b6ad8aa7688ad2420853acb359fda1c
Camila Soares <camilagoulartsoares@yahoo.com>
2025-08-05 05:07:17 -0300

    splice

---
+++ udemy/javascript/aula62/index.js
@@ -0,0 +1,15 @@
+// const nomes = ['Maria', 'Edu','Gabriel','Julia']
+
+//nomes.splice(íncide,delete,eleme)
+//índice - qual índice quero começar a mexer
+//delete quantos eu quero apagar
+
+
+
+// nomes.splice(4)
+//quero remover o índice 4
+
+const nomes = ['Ana','Edu','Caio','Fer'];
+nomes.splice(1,1) //A partir do índice 1,quero remover o elemento 2 
+
+console.log(nomes)
